adfa71a242e2be5a233ccdef328e4de8b56622be
Robin <fbzhong@gmail.com>
2011-12-04 22:04:47 +0800

    add code to git repo.

---
+++ README.md
@@ -0,0 +1,50 @@
+JSLint support for Sublime Text 2 by using jslint4java
+========================
+
+Sublime Text 2 (http://www.sublimetext.com/2) is a sophisticated text editor for code, html and prose. You'll love the slick user interface and extraordinary features.
+
+JSLint4Java (http://code.google.com/p/jslint4java/) is a java wrapper around the fabulous tool by Douglas Crockford, [jslint](http://jslint.com). It provides a simple interface for detecting potential problems in JavaScript code.
+
+This project provide a plugin to add JSLint support for Sublime Text 2.
+
+Features
+-------------
+
+- JSLint: Run JSLint (ctrl+j)
+
+- JSLint: Show JSLint Result
+
+- Highlight error line by click on the result view
+
+Requirements
+-------------
+
+- java, and make sure java has been added to PATH
+
+How to use?
+-------------
+
+- Using the Command Palette (Windows and Linux: Ctrl+Shift+P, OSX: Command+Shift+P) then search for:
+
+    - JSLint: Run JSLint (ctrl+j)
+    - JSLint: Show JSLint Result
+
+Open up a .js file and hit ctrl+j to run JSLint. An new output panel will appear giving you the JSLint results:
+
+Screenshots
+-------------
+
+![](https://github.com/fbzhong/sublime-jslint/raw/master/images/screenshot.png)
+
+License
+-------------
+
+Copyright (c) 2011, Robin Zhong (fbzhong@gmail.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ jslint.py
@@ -0,0 +1,146 @@
+import os
+import re
+import sublime
+import sublime_plugin
+from statusprocess import *
+from asyncprocess import *
+
+RESULT_VIEW_NAME = 'jslint_result_view'
+
+class ShowJslintResultCommand(sublime_plugin.WindowCommand):
+  """show jslint result"""
+  def run(self):
+    self.window.run_command("show_panel", {"panel": "output."+RESULT_VIEW_NAME})
+
+class JslintCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    file_path = self.window.active_view().file_name()
+    file_name = os.path.basename(file_path)
+    cmd = 'java -jar "' + sublime.packages_path() + '/sublime-jslint/jslint4java-2.0.1.jar" "' + file_path + '"'
+
+    self.buffered_data = ''
+    self.file_path = file_path
+    self.file_name = file_name
+    self.is_running = True
+    self.tests_panel_showed = False
+
+    self.init_tests_panel()
+
+    AsyncProcess(cmd, self)
+    StatusProcess('Starting JSLint for file ' + file_name, self)
+
+    JsLintEventListener.disabled = True
+
+  def init_tests_panel(self):
+    if not hasattr(self, 'output_view'):
+      self.output_view = self.window.get_output_panel(RESULT_VIEW_NAME)
+      self.output_view.set_name(RESULT_VIEW_NAME)
+    self.clear_test_view()
+    self.output_view.settings().set("file_path", self.file_path)
+
+  def show_tests_panel(self):
+    if self.tests_panel_showed:
+      return
+    self.window.run_command("show_panel", {"panel": "output."+RESULT_VIEW_NAME})
+    self.tests_panel_showed = True
+
+  def clear_test_view(self):
+    self.output_view.set_read_only(False)
+    edit = self.output_view.begin_edit()
+    self.output_view.erase(edit, sublime.Region(0, self.output_view.size()))
+    self.output_view.end_edit(edit)
+    self.output_view.set_read_only(True)
+
+  def append_data(self, proc, data, flush=False):
+    self.buffered_data = self.buffered_data + data.decode("utf-8")
+    str = self.buffered_data.replace(self.file_path, self.file_name).replace('\r\n', '\n').replace('\r', '\n')
+
+    if flush == False:
+      rsep_pos = str.rfind('\n')
+      if rsep_pos == -1:
+        # not found full line.
+        return
+      self.buffered_data = str[rsep_pos+1:]
+      str = str[:rsep_pos+1]
+
+    self.show_tests_panel()
+    selection_was_at_end = (len(self.output_view.sel()) == 1 and self.output_view.sel()[0] == sublime.Region(self.output_view.size()))
+    self.output_view.set_read_only(False)
+    edit = self.output_view.begin_edit()
+    self.output_view.insert(edit, self.output_view.size(), str.strip())
+    if selection_was_at_end:
+      self.output_view.show(self.output_view.size())
+    self.output_view.end_edit(edit)
+    self.output_view.set_read_only(True)
+
+    if flush:
+      self.output_view.run_command("goto_line", {"line": 1})
+
+  def update_status(self, msg, progress):
+    sublime.status_message(msg + " " + progress)
+
+  def proc_terminated(self, proc):
+    if proc.returncode == 0:
+      msg = self.file_name + ' lint free!'
+    else:
+      msg = ''
+    self.append_data(proc, msg, True)
+
+    JsLintEventListener.disabled = False
+
+
+class JsLintEventListener(sublime_plugin.EventListener):
+  """jslint event"""
+  disabled = False
+  def __init__(self):
+    self.previous_resion = None
+    self.file_view = None
+
+  def on_deactivated(self, view):
+    if view.name() != RESULT_VIEW_NAME:
+      return
+    self.previous_resion = None
+    self.file_view.erase_regions(RESULT_VIEW_NAME)
+
+  def on_selection_modified(self, view):
+    if JsLintEventListener.disabled:
+      return
+    if view.name() != RESULT_VIEW_NAME:
+      return
+    region = view.line(view.sel()[0])
+
+    # make sure call once.
+    if self.previous_resion == region:
+      return
+    self.previous_resion = region
+
+    # extract line from jslint result.
+    text = view.substr(region).split(':')
+    if len(text) < 4 or text[0] != 'jslint' or re.match('\d+', text[2]) == None or re.match('\d+', text[3]) == None:
+        return
+    line = int(text[2])
+    col = int(text[3])
+
+    # hightligh view line.
+    view.add_regions(RESULT_VIEW_NAME, [region], "comment")
+
+    # find the file view.
+    file_path = view.settings().get('file_path')
+    window = sublime.active_window()
+    file_view = None
+    for v in window.views():
+      if v.file_name() == file_path:
+        file_view = v
+        break
+    if file_view == None:
+      return
+
+    self.file_view = file_view
+    window.focus_view(file_view)
+    file_view.run_command("goto_line", {"line": line})
+    file_region = file_view.line(file_view.sel()[0])
+
+    # highlight file_view line
+    file_view.add_regions(RESULT_VIEW_NAME, [file_region], "string")
+
+
